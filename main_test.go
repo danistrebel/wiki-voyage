55b07307eadf2bbdfb912a2abdab5e1c78842a62
Daniel <strebel@google.com>
2025-02-25 09:18:46 +0000

    new stuff

---
+++ main_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestActivityEmoji(t *testing.T) {
+	tests := []struct {
+		name     string
+		activity string
+		want     string
+	}{
+		{
+			name:     "see",
+			activity: "see",
+			want:     "👀",
+		},
+		{
+			name:     "do",
+			activity: "do",
+			want:     "🤸",
+		},
+		{
+			name:     "eat",
+			activity: "eat",
+			want:     "🍽️",
+		},
+		{
+			name:     "sleep",
+			activity: "sleep",
+			want:     "🛌",
+		},
+		{
+			name:     "buy",
+			activity: "buy",
+			want:     "🛍️",
+		},
+		{
+			name:     "drink",
+			activity: "drink",
+			want:     "🍹",
+		},
+		{
+			name:     "unknown",
+			activity: "unknown",
+			want:     "",
+		},
+		{
+			name:     "empty",
+			activity: "",
+			want:     "",
+		},
+		{
+			name:     "UpperCase",
+			activity: "SEE",
+			want:     "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := activityEmoji(tt.activity); got != tt.want {
+				t.Errorf("activityEmoji() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
