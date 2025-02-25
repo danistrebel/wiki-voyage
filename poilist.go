b5829e0c8526bbaad6f2df64a682ec3947910798
Daniel <strebel@google.com>
2025-02-25 09:27:19 +0000

    new stuff

--- poilist.go
+++ poilist.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"sync"
+
+	"cloud.google.com/go/bigquery"
+	"google.golang.org/api/iterator"
 )
 
 var (
@@ -14,29 +18,74 @@ var (
 	poiCacheMu sync.RWMutex
 )
 
-func loadPointsOfInterest(city string) ([]PointOfInterest, error) {
-
-	return []PointOfInterest{
-		{
-			Title:       "Google ZRH Europaallee",
-			Description: "The Google office in Zurich is an engineering hub for artificial intelligence, machine learning, and natural language processing. The office is also home to teams working on Google products such as Gemini, Maps, and YouTube.",
-			Latitude:    47.3789437,
-			Longitude:   8.5324559,
-			Activity:    "work",
-			Icon:        "🤓",
-		},
-		{
-			Title:       "Google ZRH Brandschenkestrasse",
-			Description: "Another Google office in Zurich.",
-			Latitude:    47.365464,
-			Longitude:   8.525309,
-			Activity:    "work",
-			Icon:        "🤓",
-		},
-	}, nil
+func activityEmoji(activity string) string {
+	switch activity {
+	case "see":
+		return "👀" // Eyes
+	case "do":
+		return "🤸" // Person doing cartwheel
+	case "eat":
+		return "🍽️" // Fork and knife with plate
+	case "sleep":
+		return "🛌" // Person in bed
+	case "buy":
+		return "🛍️" // Shopping bags
+	case "drink":
+		return "🍹" // Tropical drink
+	default:
+		return "" // No emoji for unknown activity
+	}
 }
 
-const defaultCity = "Zurich"
+func limitStringLength(s string, maxLength int) (string, error) {
+	if maxLength < 0 {
+		return "", fmt.Errorf("maxLength cannot be negative")
+	}
+	if len(s) <= maxLength {
+		return s, nil
+	}
+	return s[:maxLength] + "..", nil
+}
+
+func loadPointsOfInterest(city string) ([]PointOfInterest, error) {
+	projectId := os.Getenv("PROJECT_ID")
+	if projectId == "" {
+		return nil, fmt.Errorf("Missing PROJECT_ID")
+	}
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectId)
+	query := fmt.Sprintf("SELECT * FROM `%s.wiki_voyage.points_of_interest` WHERE city = '%s'", projectId, city)
+	if err != nil {
+		return nil, fmt.Errorf("bigquery.NewClient: %w", err)
+	}
+	defer client.Close()
+	q := client.Query(query)
+	it, err := q.Read(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("query.Read: %w", err)
+	}
+	var pois []PointOfInterest
+	for {
+		var poi PointOfInterest
+		err := it.Next(&poi)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("iterator.Next: %w", err)
+		}
+		poi.Icon = activityEmoji(poi.Activity)
+		poi.Description, err = limitStringLength(poi.Description, 100)
+		if err != nil {
+			return nil, fmt.Errorf("limitStringLength: %w", err)
+		}
+		pois = append(pois, poi)
+	}
+	return pois, nil
+
+}
+
+const defaultCity = "Istanbul"
 
 func listPointsOfInterestHandler(w http.ResponseWriter, r *http.Request) {
 	MapsApiKey := os.Getenv("MAPS_KEY")
