fe5c47bfc57adec51c014c7ceac7b8e09d7e5a61
Daniel <strebel@google.com>
2025-03-17 11:14:34 +0000

    feat: frankfurt stuff

--- poilist.go
+++ poilist.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"sync"
+
+	"cloud.google.com/go/bigquery"
+	"google.golang.org/api/iterator"
 )
 
 var (
@@ -15,28 +19,40 @@ var (
 )
 
 func loadPointsOfInterest(city string) ([]PointOfInterest, error) {
+	projectId := os.Getenv("PROJECT_ID")
+	if projectId == "" {
+		log.Fatal("Missing PROJECT_ID")
+	}
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectId)
+	if err != nil {
+		return nil, fmt.Errorf("error creating BigQuery client: %w", err)
+	}
+	query := fmt.Sprintf("SELECT * FROM `%s.wiki_voyage.points_of_interest` WHERE city = '%s'", projectId, city)
+	q := client.Query(query)
+	it, err := q.Read(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("error reading query results: %w", err)
+	}
+	var pois []PointOfInterest
+	for {
+		var poi PointOfInterest
+		err := it.Next(&poi)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("error iterating query results: %w", err)
+		}
+		poi.Icon = activityEmoji(poi.Activity)
+		poi.Description = limitStringLength(poi.Description, 100)
+		pois = append(pois, poi)
+	}
+	return pois, nil
 
-	return []PointOfInterest{
-		{
-			Title:       "Google ZRH Europaallee",
-			Description: "The Google office in Zurich is an engineering hub for artificial intelligence, machine learning, and natural language processing. The office is also home to teams working on Google products such as Gemini, Maps, and YouTube.",
-			Latitude:    47.3789437,
-			Longitude:   8.5324559,
-			Activity:    "work",
-			Icon:        "🤓",
-		},
-		{
-			Title:       "Google ZRH Brandschenkestrasse",
-			Description: "Another Google office in Zurich.",
-			Latitude:    47.365464,
-			Longitude:   8.525309,
-			Activity:    "work",
-			Icon:        "🤓",
-		},
-	}, nil
 }
 
-const defaultCity = "Zurich"
+const defaultCity = "Frankfurt"
 
 func listPointsOfInterestHandler(w http.ResponseWriter, r *http.Request) {
 	MapsApiKey := os.Getenv("MAPS_KEY")
@@ -74,4 +90,34 @@ func listPointsOfInterestHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tmpl.Execute(w, data)
+}
+
+func limitStringLength(s string, maxLength int) string {
+	if maxLength <= 0 {
+		return ""
+	}
+	if len(s) <= maxLength {
+		return s
+	}
+	return s[:maxLength] + ".."
+}
+
+// activityEmoji returns the emoji corresponding to the activity type.
+func activityEmoji(activity string) string {
+	switch activity {
+	case "see":
+		return "👀"
+	case "do":
+		return "🤸"
+	case "eat":
+		return "🍽️"
+	case "sleep":
+		return "🛏️"
+	case "buy":
+		return "🛍️"
+	case "drink":
+		return "🍹"
+	default:
+		return "📍"
+	}
 }
